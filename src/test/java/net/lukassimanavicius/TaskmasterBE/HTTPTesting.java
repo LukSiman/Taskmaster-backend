6cd215f323c824b0b036f4eb95127e1c01f1719e
LukSiman <simanavicius.lukas@gmail.com>
2022-08-31 22:03:12 -0700

    added testing for get requests for categories

--- src/test/java/net/lukassimanavicius/TaskmasterBE/HTTPTesting.java
+++ src/test/java/net/lukassimanavicius/TaskmasterBE/HTTPTesting.java
@@ -1,7 +1,5 @@
 package net.lukassimanavicius.TaskmasterBE;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.lukassimanavicius.TaskmasterBE.entities.Category;
 import net.lukassimanavicius.TaskmasterBE.services.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +11,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -27,14 +24,74 @@ public class HTTPTesting {
     private CategoryService categoryService;
 
     @Test
-    public void GetCategoryTest() throws Exception{
-        // when -  action or the behaviour that we are going test
+    public void GetSingleCategoryTest() throws Exception {
         ResultActions response = mockMvc.perform(get("/categories/{id}", 1));
 
-        // then - verify the output
         response.andExpect(status().isOk())
                 .andDo(print())
                 .andExpect(jsonPath("categoryName", is("Work")));
+    }
 
+    @Test
+    public void GetSingleBadCategoryTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/categories/{id}", 99));
+
+        response.andExpect(status().isBadRequest())
+                .andDo(print()).andExpect(content().string("Bad id, please try again."));
+    }
+
+    @Test
+    public void GetSingleEmptyCategoryTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/categories/{id}", " "));
+
+        response.andExpect(status().isBadRequest())
+                .andDo(print()).andExpect(content().string("Bad id, please try again."));
+    }
+
+    @Test
+    public void GetSingleNotNumberCategoryTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/categories/{id}", "c"));
+
+        response.andExpect(status().isBadRequest())
+                .andDo(print()).andExpect(content().string("Bad id, please try again."));
+    }
+
+    @Test
+    public void GetSeveralSingleCategoryTest() throws Exception {
+        ResultActions response1 = mockMvc.perform(get("/categories/{id}", 2));
+        ResultActions response2 = mockMvc.perform(get("/categories/{id}", 4));
+        ResultActions response3 = mockMvc.perform(get("/categories/{id}", 6));
+        ResultActions response4 = mockMvc.perform(get("/categories/{id}", 9));
+        ResultActions response5 = mockMvc.perform(get("/categories/{id}", 12));
+
+
+        response1.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("categoryName", is("Entertainment")));
+
+        response2.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("categoryName", is("Fitness")));
+
+        response3.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("categoryName", is("Medical")));
+
+        response4.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("categoryName", is("Household")));
+
+        response5.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("categoryName", is("Other")));
+    }
+
+    @Test
+    public void GetAllCategoryTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/categories"));
+
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(12)));
     }
 }
