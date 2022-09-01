d70936ad7924cf0d1cd4e1d021ee882d677e02a2
LukSiman <simanavicius.lukas@gmail.com>
2022-08-31 18:39:20 -0700

    Adding the category controller

---
+++ src/main/java/net/lukassimanavicius/TaskmasterBE/controllers/CategoryController.java
@@ -0,0 +1,40 @@
+package net.lukassimanavicius.TaskmasterBE.controllers;
+
+import net.lukassimanavicius.TaskmasterBE.dto.CategoryDTO;
+import net.lukassimanavicius.TaskmasterBE.entities.Category;
+import net.lukassimanavicius.TaskmasterBE.services.CategoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private final CategoryService categoryService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    /**Return a CategoryDTO according to the requested ID*/
+    @GetMapping("{id}")
+    @ResponseBody
+    public ResponseEntity<CategoryDTO> getCategory(@PathVariable int id){
+
+        // get the category entity
+        Category category = categoryService.getCategory(id);
+
+        // convert entity to DTO
+        CategoryDTO categoryDTO = modelMapper.map(category, CategoryDTO.class);
+
+        // return the DTO as a response entity
+        return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
+    }
+}
