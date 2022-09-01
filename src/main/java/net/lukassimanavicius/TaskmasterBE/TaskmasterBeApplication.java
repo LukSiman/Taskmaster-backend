e5ee572643f29a2d39e22ccae4ea6fa91919ff7c
LukSiman <simanavicius.lukas@gmail.com>
2022-08-31 17:28:44 -0700

    Initial init

---
+++ src/main/java/net/lukassimanavicius/TaskmasterBE/TaskmasterBeApplication.java
@@ -0,0 +1,13 @@
+package net.lukassimanavicius.TaskmasterBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/lukassimanavicius/TaskmasterBE/TaskmasterBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lukassimanavicius.TaskmasterBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmasterBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
