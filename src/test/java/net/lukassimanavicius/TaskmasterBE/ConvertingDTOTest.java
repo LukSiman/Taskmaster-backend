ca552893a76ffb35ef2fd9c93378382475853b7a
LukSiman <simanavicius.lukas@gmail.com>
2022-08-31 19:03:18 -0700

    Adding tests for DTO conversion for categories

---
+++ src/test/java/net/lukassimanavicius/TaskmasterBE/ConvertingDTOTest.java
@@ -0,0 +1,36 @@
+package net.lukassimanavicius.TaskmasterBE;
+
+import net.lukassimanavicius.TaskmasterBE.dto.CategoryDTO;
+import net.lukassimanavicius.TaskmasterBE.entities.Category;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConvertingDTOTest {
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @Test
+    public void convertCategoryToDTOTest(){
+        Category category = new Category();
+        String name = "TestTest";
+        category.setCategoryName(name);
+
+        CategoryDTO categoryDTO = modelMapper.map(category, CategoryDTO.class);
+        assertEquals(name, category.getCategoryName());
+        assertEquals(category.getCategoryName(), categoryDTO.getCategoryName());
+    }
+
+    @Test
+    public void convertDTOToCategoryTest(){
+        Category category = new Category();
+        String name = "TestTest";
+        category.setCategoryName(name);
+
+        CategoryDTO categoryDTO = modelMapper.map(category, CategoryDTO.class);
+
+        assertEquals(name, categoryDTO.getCategoryName());
+        assertEquals(categoryDTO.getCategoryName(), category.getCategoryName());
+    }
+}
