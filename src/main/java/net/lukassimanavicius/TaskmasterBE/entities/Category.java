2717fafbf36b6a702393e0c326a9e70184752d74
LukSiman <simanavicius.lukas@gmail.com>
2022-08-31 18:11:13 -0700

    Created entity for the category DB table

---
+++ src/main/java/net/lukassimanavicius/TaskmasterBE/entities/Category.java
@@ -0,0 +1,21 @@
+package net.lukassimanavicius.TaskmasterBE.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "task_categories")
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Integer categoryId;
+
+    @Column(name = "category_name")
+    private String categoryName;
+}
