26a9e9abb89e57da36c2405d91def03e15b6fe69
LukSiman <simanavicius.lukas@gmail.com>
2022-09-04 19:17:15 -0700

    added HTTP tests for the task object

--- src/test/java/net/lukassimanavicius/TaskmasterBE/HTTPTesting.java
+++ src/test/java/net/lukassimanavicius/TaskmasterBE/HTTPTesting.java
@@ -94,4 +94,108 @@ public class HTTPTesting {
                 .andDo(print())
                 .andExpect(jsonPath("$.size()", is(12)));
     }
+
+
+    @Test
+    public void GetSingleTaskTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/tasks/{id}", 1));
+
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("taskName", is("Wash up")))
+                .andExpect(jsonPath("taskOrder", is(1)))
+                .andExpect(jsonPath("taskNote", is("")))
+                .andExpect(jsonPath("taskStatus", is("Completed")))
+                .andExpect(jsonPath("taskStartTime", is("08:00:00")))
+                .andExpect(jsonPath("taskEndTime", is("08:15:00")))
+                .andExpect(jsonPath("taskDate", is("2022-08-28")));
+    }
+
+    @Test
+    public void GetSingleBadTaskTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/tasks/{id}", 99));
+
+        response.andExpect(status().isBadRequest())
+                .andDo(print()).andExpect(content().string("Bad id, please try again."));
+    }
+
+    @Test
+    public void GetSingleEmptyTaskTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/tasks/{id}", " "));
+
+        response.andExpect(status().isBadRequest())
+                .andDo(print()).andExpect(content().string("Bad id, please try again."));
+    }
+
+    @Test
+    public void GetSingleNotNumberTaskTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/tasks/{id}", "c"));
+
+        response.andExpect(status().isBadRequest())
+                .andDo(print()).andExpect(content().string("Bad id, please try again."));
+    }
+
+    @Test
+    public void GetSeveralSingleTaskTest() throws Exception {
+        ResultActions response1 = mockMvc.perform(get("/tasks/{id}", 2));
+        ResultActions response2 = mockMvc.perform(get("/tasks/{id}", 8));
+        ResultActions response3 = mockMvc.perform(get("/tasks/{id}", 14));
+        ResultActions response4 = mockMvc.perform(get("/tasks/{id}", 20));
+        ResultActions response5 = mockMvc.perform(get("/tasks/{id}", 26));
+
+
+        response1.andExpect(status().isOk())
+                .andExpect(jsonPath("taskName", is("Breakfast")))
+                .andExpect(jsonPath("taskOrder", is(2)))
+                .andExpect(jsonPath("taskNote", is("Overnight oats")))
+                .andExpect(jsonPath("taskStatus", is("Completed")))
+                .andExpect(jsonPath("taskStartTime", is("08:15:00")))
+                .andExpect(jsonPath("taskEndTime", is("08:30:00")))
+                .andExpect(jsonPath("taskDate", is("2022-08-28")));
+
+        response2.andExpect(status().isOk())
+                .andExpect(jsonPath("taskName", is("Learning")))
+                .andExpect(jsonPath("taskOrder", is(8)))
+                .andExpect(jsonPath("taskNote", is("Algos chapter 11")))
+                .andExpect(jsonPath("taskStatus", is("")))
+                .andExpect(jsonPath("taskStartTime", is("20:30:00")))
+                .andExpect(jsonPath("taskEndTime", is("22:00:00")))
+                .andExpect(jsonPath("taskDate", is("2022-08-28")));
+
+        response3.andExpect(status().isOk())
+                .andExpect(jsonPath("taskName", is("Task3")))
+                .andExpect(jsonPath("taskOrder", is(3)))
+                .andExpect(jsonPath("taskNote", is("Random note")))
+                .andExpect(jsonPath("taskStatus").doesNotExist())
+                .andExpect(jsonPath("taskStartTime").doesNotExist())
+                .andExpect(jsonPath("taskEndTime").doesNotExist())
+                .andExpect(jsonPath("taskDate", is("2022-08-29")));
+
+        response4.andExpect(status().isOk())
+                .andExpect(jsonPath("taskName", is("Task4")))
+                .andExpect(jsonPath("taskOrder", is(1)))
+                .andExpect(jsonPath("taskNote").doesNotExist())
+                .andExpect(jsonPath("taskStatus").doesNotExist())
+                .andExpect(jsonPath("taskStartTime").doesNotExist())
+                .andExpect(jsonPath("taskEndTime").doesNotExist())
+                .andExpect(jsonPath("taskDate", is("2022-09-20")));
+
+        response5.andExpect(status().isOk())
+                .andExpect(jsonPath("taskName", is("Task1")))
+                .andExpect(jsonPath("taskOrder", is(1)))
+                .andExpect(jsonPath("taskNote").doesNotExist())
+                .andExpect(jsonPath("taskStatus").doesNotExist())
+                .andExpect(jsonPath("taskStartTime").doesNotExist())
+                .andExpect(jsonPath("taskEndTime").doesNotExist())
+                .andExpect(jsonPath("taskDate", is("2022-08-28")));
+    }
+
+    @Test
+    public void GetAllTasksTest() throws Exception {
+        ResultActions response = mockMvc.perform(get("/tasks"));
+
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(26)));
+    }
 }
