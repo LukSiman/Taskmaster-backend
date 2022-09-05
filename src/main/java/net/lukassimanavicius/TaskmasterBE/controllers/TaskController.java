4fb4759c0e9ec19e7fe46de335ccf2e64340e803
LukSiman <simanavicius.lukas@gmail.com>
2022-09-04 18:43:24 -0700

    added a controller for the task object

---
+++ src/main/java/net/lukassimanavicius/TaskmasterBE/controllers/TaskController.java
@@ -0,0 +1,75 @@
+package net.lukassimanavicius.TaskmasterBE.controllers;
+
+import net.lukassimanavicius.TaskmasterBE.dto.TaskDTO;
+import net.lukassimanavicius.TaskmasterBE.entities.Task;
+import net.lukassimanavicius.TaskmasterBE.services.TaskService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private final TaskService taskService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    /**
+     * Returns a list of all tasks
+     */
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<TaskDTO>> getAllTasks() {
+        // get a list of task entities
+        List<Task> tasks = taskService.getAllTasks();
+
+        // convert entities to DTO
+        List<TaskDTO> tasksDTO = new ArrayList<>();
+        for (Task task : tasks) {
+            tasksDTO.add(modelMapper.map(task, TaskDTO.class));
+        }
+
+        // return a list of DTO objects as a response entity
+        return new ResponseEntity<>(tasksDTO, HttpStatus.OK);
+    }
+
+
+    /**
+     * Returns a single task by ID
+     */
+    @GetMapping("{id}")
+    @ResponseBody
+    public ResponseEntity<TaskDTO> getTask(@PathVariable int id) {
+
+        // get the task entity
+        Task task = taskService.getSingleTask(id);
+
+        // convert entity to DTO
+        TaskDTO taskDTO = modelMapper.map(task, TaskDTO.class);
+
+        // return the DTO as a response entity
+        return new ResponseEntity<>(taskDTO, HttpStatus.OK);
+    }
+
+    /**
+     * Handles exceptions for bad IDs
+     */
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, EntityNotFoundException.class})
+    private ResponseEntity handleBadID() {
+        return ResponseEntity.badRequest().body("Bad id, please try again.");
+    }
+}
