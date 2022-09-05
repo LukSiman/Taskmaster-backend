8d0d742816c1974489b3f5eae0976a2752af2c65
LukSiman <simanavicius.lukas@gmail.com>
2022-09-04 18:42:25 -0700

    created DTO for task object

---
+++ src/main/java/net/lukassimanavicius/TaskmasterBE/dto/TaskDTO.java
@@ -0,0 +1,19 @@
+package net.lukassimanavicius.TaskmasterBE.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+public class TaskDTO {
+    private String taskName;
+    private Integer taskOrder;
+    private String taskNote;
+    private String taskStatus;
+    private LocalTime taskStartTime;
+    private LocalTime taskEndTime;
+    private LocalDate taskDate;
+}
