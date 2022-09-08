3b97a0b93017c96d6d0d767f64f6b52a2aeaeaf5
LukSiman <simanavicius.lukas@gmail.com>
2022-09-07 19:04:20 -0700

    adding saving for tasks when adding a category name

--- src/main/java/net/lukassimanavicius/TaskmasterBE/services/TaskService.java
+++ src/main/java/net/lukassimanavicius/TaskmasterBE/services/TaskService.java
@@ -1,10 +1,13 @@
 package net.lukassimanavicius.TaskmasterBE.services;
 
+import net.lukassimanavicius.TaskmasterBE.entities.Category;
 import net.lukassimanavicius.TaskmasterBE.entities.Task;
+import net.lukassimanavicius.TaskmasterBE.repositories.CategoryRepository;
 import net.lukassimanavicius.TaskmasterBE.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,8 +18,12 @@ public class TaskService {
     private final TaskRepository taskRepository;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository) {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository, CategoryRepository categoryRepository) {
         this.taskRepository = taskRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     /**
@@ -31,6 +38,35 @@ public class TaskService {
      */
     public Task getSingleTask(UUID id) {
         Task task = taskRepository.findByTaskUUID(id);
+        return task;
+    }
+
+    /**
+     * Saves the task to the database
+     */
+    @Transactional
+    public Task saveTask(Task taskToSave) {
+
+        // create a UUID for the task
+        taskToSave.setTaskUUID(UUID.randomUUID());
+
+        // extract category if task entity has it
+        if (taskToSave.getCategory() != null) {
+            // extract the category name from the request
+            String categoryName = taskToSave.getCategory().getCategoryName();
+
+            // find the category by name
+            Category category = categoryRepository.findByCategoryName(categoryName);
+
+            // extract the ID from the category
+            Integer categoryID = category.getCategoryID();
+
+            // set the category ID to the task entity
+            taskToSave.getCategory().setCategoryID(categoryID);
+        }
+
+        Task task = taskRepository.save(taskToSave);
+
         return task;
     }
 }
