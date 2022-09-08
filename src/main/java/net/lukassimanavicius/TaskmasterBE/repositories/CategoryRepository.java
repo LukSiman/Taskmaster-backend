c33f0697d7235cca9ccfa4929e2ca9e36b16047f
LukSiman <simanavicius.lukas@gmail.com>
2022-09-07 19:05:00 -0700

    adding method to find category by name

--- src/main/java/net/lukassimanavicius/TaskmasterBE/repositories/CategoryRepository.java
+++ src/main/java/net/lukassimanavicius/TaskmasterBE/repositories/CategoryRepository.java
@@ -4,4 +4,5 @@ import net.lukassimanavicius.TaskmasterBE.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Category findByCategoryName(String categoryName);
 }
